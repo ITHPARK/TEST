423aa95820212fdcee354a9a0f8aa05d04c85111
ITHPARK <ithpark1035@gmail.com>
2023-09-22 17:55:19 +0900

    타이핑 효 과 작업

---
+++ Project/resource/js/common.js
@@ -0,0 +1,38 @@
+
+  
+$(function () {
+  
+  let title = document.querySelector('.intro_txt');
+  //태그가 body에 작성된 이후에 선택자로 잡혀야 함   
+  
+   //타이핑 효과
+  let textC = "ADSDSADSA"
+  let i = 0; 
+  
+  let goTyping = setInterval(typing, 200);  
+
+  
+  function typing () {
+    
+    let word = textC[i]
+    console.log(word)
+    
+    $('.intro_txt').append(textC[i])
+    
+    i++;
+    
+    if (i == 9) { 
+      clearInterval(goTyping);    
+    }  
+    
+  }
+});
+  
+  
+
+  
+  
+  
+  
+  
+
